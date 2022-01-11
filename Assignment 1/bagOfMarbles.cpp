7ae472916839104c8ef25d2013320ccc856e7c1c
BrettAbaurrea <brett.abaurrea.9613@mail.linnbenton.edu>
2022-01-11 15:12:48 -0800

    Users can add marbles, quit the program, & see how many marbles they have left

--- Assignment 1/bagOfMarbles.cpp
+++ Assignment 1/bagOfMarbles.cpp
@@ -10,12 +10,34 @@ int main()
     int bag = 0;
     int hand = 0;
     int addMarbles = 0;
+    int i = 1;
+    char quit;
 
     cout << "how many marbles do you have? ";
     cin >> hand;
     //cout << "You have " << hand << " marbles!\n";
-    cout << "There are currently " << bag << " marbles in the bag.\nHow many would you like to add?";
-    cin >> addMarbles;
+    do{
+
+        cout << "There are currently " << bag << " marbles in the bag.\n\nHow many would you like to add? ";
+        cin >> addMarbles;
+        //i++;
+        if(addMarbles > hand){
+            cout << "\nSorry you don't have enough marbles\n";
+        }
+        else{
+            bag = bag + addMarbles;
+            hand = hand - addMarbles;
+        }
+        cout << "\nYou have " << hand << " marbles left.\n";
+        if (hand == 0){
+            cout << "Since you're out of marbles would you like to quit? Y/N  ";
+            cin >> quit;
+            if (quit == 'y' || quit == 'Y'){
+                i = 2;
+            }
+        }
+    }while(i==1);
+    
 
     return 0;
 }
