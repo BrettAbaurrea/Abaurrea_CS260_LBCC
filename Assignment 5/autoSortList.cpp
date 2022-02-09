09c6ebd9c6bfb1ac6969c2402ba52eaabb3c9936
BrettAbaurrea <brett.abaurrea.9613@mail.linnbenton.edu>
2022-02-08 22:43:53 -0800

    Added remove function

--- Assignment 5/autoSortList.cpp
+++ Assignment 5/autoSortList.cpp
@@ -18,6 +18,17 @@ class AutoSort{
     private:
         //Creates front (start) position of node
         Node *front;
+
+        //Wanting to look at item in list that came before
+        Node *findPrevItem(int value){
+            Node *future = front->next;
+            Node *current = front;
+            while(future != nullptr && value > future->value){
+                current = future;
+                future = future->next;
+            }
+            return current;
+        }
     
     public:
         //Constructor
@@ -38,6 +49,37 @@ class AutoSort{
                 Node * newNode = new Node{newValue, nullptr};
                 front = newNode;
             }
+            //Insert value into middle of list
+            else if (newValue >= front->value){
+                //Go to next available position to enter newValue
+                Node *previous = findPrevItem(newValue);
+                //Now insert that (new)value
+                Node *newNode = new Node{newValue, previous->next};
+                previous->next = newNode;
+            }
+            
+        }
+        //Remove from list
+        int remove(int oldValue){
+            int currentValue = -1;
+
+            //If list is empty do nothing (return -2 which means empty list)
+            if(front == nullptr){
+                currentValue = -2;
+            }
+            //If the current position is a value, delete it
+            else if(front->value == oldValue){
+                Node * current = front;
+                currentValue = current->value;
+                front = front->next;
+                delete current;
+            }
+            else{
+                Node *previous = findPrevItem(oldValue);
+                Node *current = previous->next;
+                delete current;
+            }
+            return currentValue;
         }
 
         string printScreen(){
--- Assignment 5/testAutoSortList.cpp
+++ Assignment 5/testAutoSortList.cpp
@@ -16,6 +16,28 @@ int main(){
     cout << "Created List: " << sortedList->printScreen() << endl;
 
     //And an item to empty list
-    sortedList->add(22); //Adds 22 into list
+    sortedList->add(1); //Adds 1 into an empty list (starts a list)
     cout << "List with 1 item: " << sortedList->printScreen() << endl;
+
+    //Add item to end and print list
+    sortedList->add(99); //Adds 99 into end of list
+    cout << "List with end item: " << sortedList->printScreen() << endl;
+
+    //add to middle and print list
+    sortedList->add(3); //Adds 3 into list
+    cout << "List with middle item: " << sortedList->printScreen() << endl;
+
+    //remove middle
+    sortedList->remove(3); //removes 3 (since thats the middle value)
+    cout << "List with missing middle item: " << sortedList->printScreen() << endl;
+
+    //remove front
+    sortedList->remove(1); //removes 1 (since thats the front value)
+    cout << "List with missing front item: " << sortedList->printScreen() << endl;
+
+    //remove end
+    sortedList->remove(99); //removes 99 (since thats the end value)
+    cout << "List with missing end item: " << sortedList->printScreen() << endl;
+
+
 }
