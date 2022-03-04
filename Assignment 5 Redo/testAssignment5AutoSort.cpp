7878c5597eafbca7ca554e23ce53b5e5b065b799
BrettAbaurrea <brett.abaurrea.9613@mail.linnbenton.edu>
2022-03-04 13:10:15 -0800

    fixed a bug that wouldn't allow it to go back to insert once in remove function

--- Assignment 5 Redo/testAssignment5AutoSort.cpp
+++ Assignment 5 Redo/testAssignment5AutoSort.cpp
@@ -21,35 +21,38 @@ int main(){
     AutoSorter *autoSortedList = new AutoSorter();
     cout << "Current Auto Sorted List:  [ " << autoSortedList -> printScreen() << "]" << endl << endl;
 
-    //inserts user entered value and sorts it automatically
-    while (continueTest == 1){
-    cout << "What value would you like to insert? ";
-    cin >> num;
-    autoSortedList -> insertValue(num);     //ERROR I for some reason cannot enter '1' more than once
-    cout << "insert (" << num <<")\n";
-    cout << "Current Auto Sorted List:  [ " << autoSortedList -> printScreen() << "]" << endl << endl;
-    cout << "1 to insert more, 2 to remove some, 3 to search, 4 to quit: ";
-    cin >> continueTest;
-    }
-    //remove user entered value IF repeated
-    while (continueTest == 2){
-        cout << "What value would you like to remove? ";
+    
+    while (continueTest < 4){
+        //inserts user entered value and sorts it automatically
+        while (continueTest == 1){
+        cout << "What value would you like to insert? ";
         cin >> num;
-        autoSortedList -> removeFirstInstance(num); //if num isnt in list the program just quits    //ERROR if number isn't on the list it just removes a random value ? im unsure why 
-        cout << "remove (" << num <<")\n";
+        autoSortedList -> insertValue(num);     //ERROR I for some reason cannot enter '1' more than once
+        cout << "insert (" << num <<")\n";
         cout << "Current Auto Sorted List:  [ " << autoSortedList -> printScreen() << "]" << endl << endl;
         cout << "1 to insert more, 2 to remove some, 3 to search, 4 to quit: ";
         cin >> continueTest;
-    }
-    //search for user entered value
-    while (continueTest == 3){
-        cout << "What value would you like to search? ";
-        cin >> num;
-        autoSortedList -> atIndex(num); //if num isnt in list the program just quits    //ERROR if number isn't on the list it just removes a random value ? im unsure why 
-        cout << "value (" << num <<") is at index: " << autoSortedList -> atIndex(num) << endl;
-        cout << "Current Auto Sorted List:  [ " << autoSortedList -> printScreen() << "]" << endl << endl;
-        cout << "1 to insert more, 2 to remove some, 3 to search, 4 to quit: ";
-        cin >> continueTest;
+        }
+        //remove user entered value IF repeated
+        while (continueTest == 2){
+            cout << "What value would you like to remove? ";
+            cin >> num;
+            autoSortedList -> removeFirstInstance(num); //if num isnt in list the program just quits    //ERROR if number isn't on the list it just removes a random value ? im unsure why 
+            cout << "remove (" << num <<")\n";
+            cout << "Current Auto Sorted List:  [ " << autoSortedList -> printScreen() << "]" << endl << endl;
+            cout << "1 to insert more, 2 to remove some, 3 to search, 4 to quit: ";
+            cin >> continueTest;
+        }
+        //search for user entered value
+        while (continueTest == 3){
+            cout << "What value would you like to search? ";
+            cin >> num;
+            autoSortedList -> atIndex(num); //if num isnt in list the program just quits    //ERROR if number isn't on the list it just removes a random value ? im unsure why 
+            cout << "value (" << num <<") is at index: " << autoSortedList -> atIndex(num) << endl;
+            cout << "Current Auto Sorted List:  [ " << autoSortedList -> printScreen() << "]" << endl << endl;
+            cout << "1 to insert more, 2 to remove some, 3 to search, 4 to quit: ";
+            cin >> continueTest;
+        }
     }
 
     return 0;
