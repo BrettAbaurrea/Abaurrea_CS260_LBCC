94f2ec54ebbabc48b39532c5aa0bb4b31d3d58c3
BrettAbaurrea <brett.abaurrea.9613@mail.linnbenton.edu>
2022-03-05 10:54:58 -0800

    implimented binary search tree to find average age in country

---
+++ Assignment 6/binarySearchTreeCountryAge.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+#include "binaryTreeAgeNode.h"
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+void inOrderTraversal(AgeNode *root){
+    if (root == nullptr){   
+        return;
+    }
+    inOrderTraversal(root -> up);
+    cout << root -> value << ", ";
+    inOrderTraversal(root -> down);
+}
+
+int main(){
+
+    cout << "Let's find the average age of a country!\n\n";
+
+    /*
+    * Declare AgeNode Values
+    */
+
+   //intial age 
+   AgeNode *root = new AgeNode{30, nullptr. nullptr};   //average human is 28 (i rounded to 30 bc its prettier)
+   //"level" 2
+   AgeNode *ageUp = new AgeNode{40, nullptr, nullptr};  //moving up (left) gets you bigger number
+   AgeNode *ageDown = new AgeNode{20, nullptr, nullptr};    //movie down (right) gets you a smaller number
+   //"level" 3
+   AgeNode *ageUpUp = new AgeNode{45, nullptr, nullptr};    //larger than 30 & 40
+   AgeNode *ageUpDown = new AgeNode{35, nullptr, nullptr};  //larger than 30 BUT less than 40 
+   AgeNode *ageDownDown = new AgeNode{15, nullptr, nullptr};    //less than 30 & 20
+   AgeNode *ageDownUp = new AgeNode{25, nullptr, nullptr};  //less than 30 BUT larger than 20
+   //"level" 4
+   //up
+   AgeNode *ageUpUpUp = new AgeNode{48, nullptr, nullptr};  //larger than 30 & 40 & 45
+   AgeNode *ageUpUpDown = new AgeNode{43, nullptr, nullptr};    //Larger than 30 & 40 BUT less than 45
+   AgeNode *ageUpDownUp = new AgeNode{38, nullptr, nullptr};    //larger than 30 BUT less than 40 BUT larger than 35
+   AgeNode *ageUpDownDown = new AgeNode{33, nullptr, nullptr};  //larger than 30 BUT less than 40 & 35
+   //down
+   AgeNode *ageDownDownDown = new AgeNode{14, nullptr, nullptr};    //less than 30 & 20 & 15
+   AgeNode *ageDownDownUp = new AgeNode {18, nullptr, nullptr}; //less than 30 & 20 BUT larger than 15
+   AgeNode *ageDownUpDown = new AgeNode {23, nullptr, nullptr}; //less than 30 & 25 BUT larger than 20
+   AgeNode *ageDownUpUp = new AgeNode {28, nullptr, nullptr};   //less than 30 BUT larger than 25 & 20
+
+   //Make sure nodes do the right thing
+   //lv2
+   root->up = ageUp;
+   root->down = ageDown;
+   //lv3
+   //node goes up
+   ageUp->up = ageUpUp;
+   ageDown->up = ageDownUp;
+   //node goes down
+   ageUp->down = ageUpDown;
+   ageDown->down = ageDownDown;
+   //lv4
+   //node goes up
+   ageUpUp->up = ageUpUpUp;
+   ageUpDown->up = ageUpDownUp;
+   ageDownUp->up = ageDownUpUp;
+   ageDownDown->up = ageDownDownUp;
+   //node goes down
+   ageUpUp->down = ageUpUpDown;
+   ageUpDown->down = ageUpDownDown;
+   ageDownDown->down = ageDownDownDown;
+   ageDownUp->down = ageDownUpDown;
+
+
+   //test to verify it all works
+   
+
+    //inOrderTraversal(root);
+    cout << endl;
+    
+    return 0;
+}
---
+++ Assignment 6/binaryTreeAgeNode.h
@@ -0,0 +1,5 @@
+struct AgeNode{
+    int value;
+    AgeNode *up;     //greater than (left)
+    AgeNode *down;   //less than (right)
+};
